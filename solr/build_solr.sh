4d7d8c9b3a8018cb0fece93b6540e0f5e1cb9dfd
cho-hiroshi <119351@supinfo.com>
2018-08-29 06:19:23 +0900

    Update build_solr.sh

--- solr/build_solr.sh
+++ solr/build_solr.sh
@@ -1,5 +1,7 @@
 mkdir -p hackson
 mkdir -p import 
 docker run -p 8983:8983 -v hackson:/opt/solr/server/solr/hackson -v import:/opt/solr/import solr /bin/bash
-docker ps -a | grep 8983
-
+##docker ps -a | grep 8983
+container=`docker ps -a | grep 8983 | awk -F ' ' '{print $1}'`
+docker start $container
+docker exec -it $container /bin/bash
